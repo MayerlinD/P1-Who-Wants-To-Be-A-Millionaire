3304641bcb847d0064021698a6f525f3b32628a2
Mayerlin Duluc <may_duluc@hotmail.com>
2022-09-13 20:38:19 -0400

    Added game audio and created their query selectors

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const getStarted = document.querySelector('.openModal');
 const modal = document.getElementById('modal');
 const closeBtn = document.querySelector("#close");
 const beginGameBtn = document.getElementById('beginGame');
+const letsPlayAudio = document.getElementById('lets-play');
+const thinkingAudio = document.getElementById('easy');
+const wrongAnswerAudio = document.getElementById('wrong-answer');
+const correctAnswerAudio = document.getElementById('correct-answer');
 
 
 //Questions array
@@ -23,61 +27,102 @@ const closeModal = () => {
   function getRandomItem(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const question = arr[randomIndex];
-    return question.prompt;
+    questions.push(question.prompt);
 }
 
 
 //Objects
 const questions100 = [
     {
-        prompt: 'According to a popular saying, those facing the consequences of their actions are "paying the" what?\nA: Meter\nB: Taxman\nC: Piper\nD: Alimony',
+        prompt: 'According to a popular saying, those facing the consequences of their actions are "paying the" what?',
+        a: 'Meter',
+        b: 'Taxman',
+        c: 'Piper',
+        d: 'Alimony',
         answer: 'C'
     },
     {
-        prompt: 'What car company launched the ad slogan "Drivers Wanted" in the 1990s?\nA: Volkswagen\nB: Audi\nC: BMW\nD: Mercedes',
+        prompt: 'What car company launched the ad slogan "Drivers Wanted" in the 1990s?',
+        a: 'Volkswagen',
+        b: 'Audi',
+        c: 'BMW',
+        d: 'Mercedes',
         answer: 'A'
     },
     {
-        prompt: 'Which Disney character famously leaves a glass slipper behind at a royal ball?\nA: Elsa\nB: Cinderella\nC: Snow White\nD: Sleeping Beauty',
+        prompt: 'Which Disney character famously leaves a glass slipper behind at a royal ball?',
+        a: 'Elsa',
+        b: 'Cinderella',
+        c: 'Snow White',
+        d: 'Sleeping Beauty',
         answer: 'B'
     }
 ]
 
-console.log(getRandomItem(questions100));
+getRandomItem(questions100);
+console.log(questions)
 
 const questions200 = [
     {
-        prompt: 'At the end of a traditional Christian wedding, what does the bride throw to her female guests?\nA: A handful of rice\nB: A ring\nC: A cement block\nD: A bouquet of flowers',
+        prompt: 'At the end of a traditional Christian wedding, what does the bride throw to her female guests?',
+        a: 'A handful of rice',
+        b: 'A ring',
+        c: 'A cement block',
+        d: 'A bouquet of flowers',
         answer: 'D'
     },
     {
-        prompt: "According to a common piece of advice, 'Don't take any wooden' what?\nA: Nickels\nB: Teeth\nC: Spoons\nD:Logs",
+        prompt: "According to a common piece of advice, 'Don't take any wooden' what?",
+        a: 'Nickels',
+        b: 'Teeth',
+        c: 'Spoons',
+        d:'Logs',
         answer: 'A'
     },
     {
-        prompt: 'What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?\nA: Hangar\nB: Terminal\nC: Concourse\nD: Carousel',
+        prompt: 'What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?',
+        a: 'Hangar',
+        b: 'Terminal',
+        c: 'Concourse',
+        d: 'Carousel',
         answer: 'D'
     }
 ]
 
 const questions300 = [
     {
-        prompt: 'Which of these expressions refers to a tall tale or unbelievable story?\nA: Cock & bull\nB: Bull & bear\nC: Cat & dog\nD: Giraffe & emu',
+        prompt: 'Which of these expressions refers to a tall tale or unbelievable story?',
+        a: 'Cock & bull',
+        b: 'Bull & bear',
+        c: 'Cat & dog',
+        d: 'Giraffe & emu',
         answer: 'A'
     },
     {
-        promp: 'Which of these types of footwear are usually at least 25 inches long and up to 12 inches wide?\nA: In-line skates\nB: Hiking boots\nC: Galoshes\nD: Snowshoes',
+        prompt: 'Which of these types of footwear are usually at least 25 inches long and up to 12 inches wide?',
+        a: 'In-line skates',
+        b: 'Hiking boots',
+        c: 'Galoshes',
+        d: 'Snowshoes',
         answer: 'D'
     },
     {
-        prompt: 'According to a popular phrase, if things are going well for you, "the world is your" what?\nA: Anenome\nB: Mussel\nC: Oyster\nD: Shrimp',
+        prompt: 'According to a popular phrase, if things are going well for you, "the world is your" what?',
+        a: 'Anenome',
+        b: 'Mussel',
+        c: 'Oyster',
+        d: 'Shrimp',
         answer: 'C'
     }
 ]
 
 questions500 = [
     {
-        prompt: 'Longtime pals Matt Damon and Ben Affleck attended highschool together in a suburb of what city?\nA: Atlanta\nB: Chicago\nC: Detroit\nD: Boston',
+        prompt: 'Longtime pals Matt Damon and Ben Affleck attended highschool together in a suburb of what city?',
+        a: 'Atlanta',
+        b: 'Chicago',
+        c: 'Detroit',
+        d: 'Boston',
         answer: 'D'
     },
     {
